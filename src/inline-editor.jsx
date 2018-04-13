2972335831d3d53b754b405e5a86c9d1b8237f39
Mark McGranaghan <mmcgrana@gmail.com>
2018-04-13 14:26:57 -0700

    Well that's something

--- src/inline-editor.jsx
+++ src/inline-editor.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import {Editor} from 'draft-js'
-import Measure from 'react-measure'
 import { CARD_EDITOR_CHANGED, CARD_SELECTED, CLEAR_SELECTIONS } from './action-types'
 
 const styles = {
@@ -38,24 +37,24 @@ class InlineEditorPresentation extends React.Component {
       this.focus()
     }
   }
+
+  componentDidUpdate() {
+    console.log('update.refs', this.refs.wrapper.clientWidth, this.refs.wrapper.clientHeight, this.refs.wrapper.getBoundingClientRect().width)
+  }
+
   render() {
     return (
-      <Measure
-        bounds
-        onResize={(contentRect) => {
-          console.log('onResize', contentRect)
-        }}>
-        {({ measureRef }) =>
-          <div ref={measureRef} style={styles} onClick={this.focus}>
-            <Editor
-              editorState={this.state.editorState}
-              onChange={this.onChange}
-              ref='editor'
-            />
-          </div>
-        }
-      </Measure>
-    );
+      <div
+        style={styles}
+        onClick={this.focus}
+        ref='wrapper'>
+        <Editor
+          editorState={this.state.editorState}
+          onChange={this.onChange}
+          ref='editor'
+        />
+      </div>
+    )
   }
 }
 
