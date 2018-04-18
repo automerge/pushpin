93e34ebcdeb6662470d42827b67c8fbf855f2524
Mark McGranaghan <mmcgrana@gmail.com>
2018-04-17 17:23:32 -0700

    Sketchy but it does delete

--- src/index.jsx
+++ src/index.jsx
@@ -4,8 +4,20 @@ import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 
 import { RootState, Reducer } from './model'
-import { INITIALIZE_IF_EMPTY } from './action-types'
+import { INITIALIZE_IF_EMPTY, CARD_DELETED } from './action-types'
 import Board from './board'
+
+const onKeyDown = (e, store) => {
+  if (e.key !== 'Backspace') {
+    return
+  }
+  const state = store.getState()
+  state.get('cards').forEach((card, idx) => {
+    if (card.get('selected') && (card.get('type') !== 'text')) {
+      store.dispatch({ type: CARD_DELETED, id: card.get('id') })
+    }
+  })
+}
 
 const init = () => {
   const store = createStore(Reducer, RootState)
@@ -16,6 +28,7 @@ const init = () => {
     </Provider>,
     document.getElementById('container')
   )
+  document.addEventListener('keydown', (e) => { onKeyDown(e, store) })
 }
 
 init()
