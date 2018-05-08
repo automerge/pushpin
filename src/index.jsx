96fc627fe3b9df92231b39f265f518d2673e34ef
Peter van Hardenberg <pvh@pvh.ca>
2018-05-08 15:43:27 -0700

    last of the lint warnings

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ipcRenderer } from 'electron'
 
 import Loop from './loop'
 import App from './components/app'
 import * as Model from './model'
-import { ipcRenderer } from 'electron'
 
 // The debug module wants to cache the env['DEBUG'] config, but they get it
 // wrong, at least for the render process. Delete the attempted cache so it
 // doesn't confuse future instances.
 localStorage.removeItem('debug')
 
-ipcRenderer.on("newDocument", () => {
+ipcRenderer.on('newDocument', () => {
   Loop.dispatch(Model.newDocument)
 })
 
