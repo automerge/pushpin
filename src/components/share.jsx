49c0d8a4cd83a46a159a8bc128c287d0a78b3f39
Roshan Choxi <roshan.choxi@gmail.com>
2018-05-17 16:24:51 -0500

    Add tabs to Share

--- src/components/share.jsx
+++ src/components/share.jsx
@@ -13,7 +13,13 @@ export default class Share extends React.PureComponent {
     ).isRequired
   }
 
-  render() {
+  constructor() {
+    super()
+
+    this.state = { tab: "contacts" }
+  }
+
+  renderContacts() {
     const authors = Object.keys(this.props.authors).map(id => {
       const author = this.props.authors[id]
       return <Contact key={id} name={author.name} avatar={author.avatar} actions={["unshare"]} />
@@ -25,7 +31,7 @@ export default class Share extends React.PureComponent {
     })
 
     return (
-      <div className='Share'>
+      <div>
         <h6>On Board</h6>
         <div className='Share__section'>
           <div className='Share__authors'> 
@@ -39,6 +45,49 @@ export default class Share extends React.PureComponent {
             { contacts } 
           </div>
         </div>
+      </div>
+    )
+  }
+
+  renderNotifications() {
+    return (
+      <h1>Notifications</h1>
+    )
+  }
+
+  tabClasses(name) {
+    if(this.state.tab === name)
+      return 'Share__tabs__tab Share__tabs__tab--active'
+    else
+      return 'Share__tabs__tab'
+  }
+
+  render() {
+    let body
+
+    if(this.state.tab === 'contacts')
+      body = this.renderContacts()
+    else if(this.state.tab === 'notifications')
+      body = this.renderNotifications()
+
+    return (
+      <div className='Share'>
+        <div className='Share__tabs'>
+          <div 
+            className={this.tabClasses('contacts')}
+            onClick={() => this.setState({ tab: 'contacts' })}
+          >
+            <i className='fa fa-group' /> Contacts
+          </div>
+          <div 
+            className={this.tabClasses('notifications')}
+            onClick={() => this.setState({ tab: 'notifications' })}
+          >
+            Notifications
+          </div>
+        </div>
+
+        { body }
       </div>
     )
   }
