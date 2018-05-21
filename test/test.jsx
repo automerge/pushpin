06db5af1579bbf76e584305555196d182bbc578a
Peter van Hardenberg <pvh@pvh.ca>
2018-05-21 12:37:48 -0700

    adding mocha / enzyme to the project

---
+++ test/test.jsx
@@ -0,0 +1,41 @@
+import assert from 'assert'
+import { configure, shallow, mount, render } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-15'
+import * as React from 'react'
+
+import * as Card from '../src/components/card'
+
+configure({ adapter: new Adapter() })
+
+describe('Array', () => {
+  describe('#indexOf()', () => {
+    it('should return -1 when the value is not present', () => {
+      assert.equal([1, 2, 3].indexOf(4), -1)
+    })
+  })
+})
+
+describe('<Card />', () => {
+  describe('should be instantiable', () => {
+    const props = {
+      card: {
+        type: 'text',
+        id: 1,
+        x: 0,
+        y: 0,
+        height: 10,
+        width: 24,
+        text: ''
+      },
+      dragState: {
+        moveX: 0,
+        moveY: 0,
+        resizeWidth: 48,
+        resizeHeight: 48
+      }
+    }
+
+    const wrapper = shallow(<Card.default {...props} />)
+    assert.equal(wrapper, wrapper)
+  })
+})
