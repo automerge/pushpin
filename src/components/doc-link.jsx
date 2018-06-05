f71591cb0b5c49e3bd0880dca799d6acd71d9b74
Roshan Choxi <roshan.choxi@gmail.com>
2018-06-05 12:14:05 +0200

    Add viewed docs menu

---
+++ src/components/doc-link.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import ContentTypes from '../content-types'
+
+export default class DocLink extends React.PureComponent {
+  static propTypes = {
+    onClick: PropTypes.func
+  }
+
+  static defaultProps = {
+    onClick: () => {}
+  }
+
+  constructor() {
+    super()
+
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(e) {
+    this.props.onClick(this.props.docId, e)
+  }
+
+  render() {
+    return (
+      <div className="DocLink" onClick={this.handleClick}>
+        <i className="fa fa-files-o" style={{background: this.props.doc.backgroundColor}} />
+        <div className="DocLink__title">{ this.props.doc.title }</div>
+      </div>
+    )
+  }
+}
+
+ContentTypes.register({
+  component: DocLink,
+  type: 'doc-link',
+  name: 'Document Link',
+  icon: 'sticky-note',
+  unlisted: true,
+})
