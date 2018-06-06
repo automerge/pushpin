dfd177c9bcc34c017f45a2f5bbd21ab965c7e81e
Roshan Choxi <roshan.choxi@gmail.com>
2018-06-06 10:49:53 +0200

    click on cards to full screen

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -197,8 +197,7 @@ export default class Board extends React.PureComponent {
   }
 
   onCardDoubleClicked(e, card) {
-    // we might want to do more here later, so let's keep the infrastructure
-    // symmetrical with onCardClicked.
+    window.location = card.url
     e.stopPropagation()
   }
 
