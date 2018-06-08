2c8c62c03b640bb1c00a0d53a8edf1a9e3d7e07d
Peter van Hardenberg <pvh@pvh.ca>
2018-06-08 12:57:15 +0200

    move toggle to new boilerplate

--- src/components/toggle.jsx
+++ src/components/toggle.jsx
@@ -19,11 +19,26 @@ export default class Toggle extends React.PureComponent {
     this.flipToggle = this.flipToggle.bind(this)
   }
 
-  componentWillMount() {
+  // This is the New Boilerplate
+  componentWillMount = () => this.refreshHandle(this.props.docId)
+  componentWillUnmount = () => window.hm.release(this.handle)
+  componentDidUpdate = (prevProps, prevState, snapshot) => {
+    if (prevProps.docId !== this.props.docId) {
+      this.refreshHandle(this.props.docId)
+    }
+  }
+
+  refreshHandle = (docId) => {
+    if (this.handle) {
+      window.hm.release(this.handle)
+    }
     this.handle = window.hm.openHandle(this.props.docId)
-    this.handle.onChange((doc) => {
-      this.setState({ ...doc })
-    })
+    this.handle.onChange(this.onChange)
+  }
+
+  // this should be overridden by components which care
+  onChange = (doc) => {
+    this.setState({ ...doc })
   }
 
   flipToggle() {
