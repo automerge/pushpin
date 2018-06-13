d51a4a67d0e2f343357d7161671e0ee6385ffb57
Mark McGranaghan <mmcgrana@gmail.com>
2018-06-13 04:25:34 -0700

    We only need a boolean for Content

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -53,7 +53,7 @@ export default class Content extends React.PureComponent {
   }
 
   onChange = (doc) => {
-    this.setState({ doc })
+    this.setState({ hasDoc: true })
   }
 
   filterProps = (props) => {
@@ -83,7 +83,7 @@ export default class Content extends React.PureComponent {
       return missingType(type)
     }
 
-    if (!this.state.doc) {
+    if (!this.state.hasDoc) {
       return null
     }
 
