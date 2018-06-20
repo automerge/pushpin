e4a6fb38eb9549ad2b5ad9a786579f8cd65d5d9c
Mark McGranaghan <mmcgrana@gmail.com>
2018-06-20 10:18:55 -0700

    Store data in Electron-standard location, outside app

--- src/constants.js
+++ src/constants.js
@@ -1,13 +1,23 @@
 import Path from 'path'
+import electron from 'electron'
 
 export const IMAGE_DIALOG_OPTIONS = {
   properties: ['openFile'],
   filters: [{ name: 'Images', extensions: ['png', 'jpg', 'jpeg', 'gif'] }]
 }
 
-export const USER = process.env.NAME || 'userA'
+// Prefer NAME if explicitly set.
+// Otherwise look for OS-level USER (Mac / Linux) or USERNAME (Windows.)
+export const USER = process.env.NAME ||
+                    process.env.USER ||
+                    process.env.USERNAME
+if (!USER) {
+  throw new Error('No user name found')
+}
 
-export const USER_PATH = Path.join('.', 'data', USER)
+// We want these constants available from both the main and render threads.
+const app = electron.app || electron.remote.app
+export const USER_PATH = Path.join(app.getPath('userData'), 'pushpin-v01', USER)
 
 export const WORKSPACE_URL_PATH = Path.join(USER_PATH, 'workspace-id.json')
 export const HYPERMERGE_PATH = Path.join(USER_PATH, 'hypermerge')
