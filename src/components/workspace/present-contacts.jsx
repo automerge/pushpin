a390cd0a71822d37467192cfe4a0083071911b32
Peter van Hardenberg <pvh@pvh.ca>
2018-06-21 12:12:56 -0700

    implement document presence

--- src/components/workspace/present-contacts.jsx
+++ src/components/workspace/present-contacts.jsx
@@ -12,7 +12,8 @@ export default class PresentContacts extends React.PureComponent {
     currentDocUrl: PropTypes.string.isRequired
   }
 
-  state = {}
+  state = { presentContacts: {} }
+  presentContactsTimerId = {}
 
   // This is the New Boilerplate
   componentWillMount = () => this.refreshHandle(this.props.currentDocUrl)
@@ -38,13 +39,38 @@ export default class PresentContacts extends React.PureComponent {
 
   onMessage = ({ msg, peer }) => {
     console.log(msg, peer)
+    const { contact } = msg
+    if (contact) {
+      this.contactPresent(contact)
+      clearTimeout(this.presentContactsTimerId[contact])
+      this.presentContactsTimerId[contact]
+        = setTimeout(this.contactAbsent(contact), 11000)
+    }
+  }
+
+  contactPresent = (contact) => {
+    if (!this.state.presentContacts[contact]) {
+      this.setState({ presentContacts:
+        { ...this.state.presentContacts, [contact]: true } })
+    }
+  }
+
+  // note this is a function that returns a function for setTimeout to use
+  contactAbsent = (contact) => () => {
+    if (this.state.presentContacts[contact]) {
+      // destructure to make a copy rather than mutating the old one
+      const presentContacts = { ...this.state.presentContacts }
+      delete presentContacts[contact]
+      this.setState({ presentContacts })
+    }
   }
 
   render = () => {
     log('render')
 
-    const contacts = (this.state.authorIds || []).map((contactId) =>
-      <Content context="title-bar" url={createDocumentLink('contact', contactId)} />)
+    const present = Object.keys(this.state.presentContacts)
+    const contacts = present.map((contactId) =>
+      <Content key={contactId} context="title-bar" url={createDocumentLink('contact', contactId)} />)
 
     return (
       <div className="PresentContacts">
