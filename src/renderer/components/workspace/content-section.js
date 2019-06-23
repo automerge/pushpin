833c0fdfedb78a33efb95d13e321355e2136a432
Peter van Hardenberg <pvh@pvh.ca>
2019-06-23 18:40:47 +0200

    rescue orphaned commits

--- src/renderer/components/workspace/content-section.js
+++ src/renderer/components/workspace/content-section.js
@@ -1,22 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import Content from '../content'
-import Actions from './actions'
+import ListMenuItem from './list-menu-item'
 
 export default function ContentSection({ name, label, actions, items }) {
-  const renderedItems = items.map((item) => {
-    const { url } = item
-    const classes = item.selected ? 'ListMenu__item ListMenu__item--selected' : 'ListMenu__item'
-
-    return (
-      <div key={url} className={classes}>
-        <Actions url={url} actions={actions}>
-          <Content context="list" url={url} />
-        </Actions>
-      </div>
-    )
-  })
+  const renderedItems = items.map(({ url, selected }) =>
+    <ListMenuItem key={url} contentUrl={url} actions={actions} selected={selected} />)
 
   const labelPartial = label ? <div className="ListMenu__segment">{label}</div> : null
 
