88f451bda12cf83a8b96b7579e9fe7788b7fe207
Jeff Peterson <jeff@yak.sh>
2019-07-01 15:02:57 -0600

    Convert BoardContextMenu to function component
    Removes implicit any's

--- src/renderer/components/board/BoardContextMenu.tsx
+++ src/renderer/components/board/BoardContextMenu.tsx
@@ -1,59 +1,54 @@
 import React from 'react'
-import Debug from 'debug'
 import { ContextMenu, MenuItem as ContextMenuItem } from 'react-contextmenu'
 import classNames from 'classnames'
 
 import ColorPicker from '../ColorPicker'
 import './ContextMenu.css'
-
-const log = Debug('pushpin:board-context-menu')
+import { LookupResult } from '../../ContentTypes'
 
 interface Props {
-  contentTypes: any[], // content-types isn't upgraded on my current branch
-  addContent: (e, any) => void,
-  onShowContextMenu: (e: Event) => void,
+  contentTypes: LookupResult[],
   backgroundColor: string,
   backgroundColors: string[],
-  changeBackgroundColor: (color: string) => void
+  addContent(e: React.MouseEvent | React.TouchEvent, contentType: LookupResult): void,
+  onShowContextMenu(e: Event): void,
+  changeBackgroundColor(color: string): void
 }
 
-export default class BoardContextMenu extends React.PureComponent<Props> {
-  render = () => {
-    log('render')
+export default function BoardContextMenu(props: Props) {
+  const createMenuItems = props.contentTypes.map((contentType) => (
+    <ContextMenuItem
+      key={contentType.type}
+      onClick={(e) => props.addContent(e, contentType)}
+    >
+      <div className="ContextMenu__iconBounding ContextMenu__iconBounding--note">
+        <i className={classNames('fa', `fa-${contentType.icon}`)} />
+      </div>
+      <span className="ContextMenu__label">{contentType.name}</span>
+    </ContextMenuItem>
+  ))
 
-    const createMenuItems = this.props.contentTypes.map((contentType) => (
-      <ContextMenuItem
-        key={contentType.type}
-        onClick={(e) => { this.props.addContent(e, contentType) }}
-      >
-        <div className="ContextMenu__iconBounding ContextMenu__iconBounding--note">
-          <i className={classNames('fa', `fa-${contentType.icon}`)} />
-        </div>
-        <span className="ContextMenu__label">{contentType.name}</span>
-      </ContextMenuItem>
-    ))
+  return (
+    <ContextMenu
+      id="BoardMenu"
+      onShow={props.onShowContextMenu}
+      className="ContextMenu"
+    >
+      <div className="ContextMenu__section">
+        {createMenuItems}
+      </div>
 
-    return (
-      <ContextMenu
-        id="BoardMenu"
-        onShow={this.props.onShowContextMenu}
-        className="ContextMenu"
-      >
-        <div className="ContextMenu__section">
-          {createMenuItems}
-        </div>
-        <div className="ContextMenu__section">
-          <h6>Board Color</h6>
-          <div className="ContextMenu__divider" />
-          <ContextMenuItem>
-            <ColorPicker
-              color={this.props.backgroundColor}
-              colors={this.props.backgroundColors}
-              onChangeComplete={this.props.changeBackgroundColor}
-            />
-          </ContextMenuItem>
-        </div>
-      </ContextMenu>
-    )
-  }
+      <div className="ContextMenu__section">
+        <h6>Board Color</h6>
+        <div className="ContextMenu__divider" />
+        <ContextMenuItem>
+          <ColorPicker
+            color={props.backgroundColor}
+            colors={props.backgroundColors}
+            onChangeComplete={props.changeBackgroundColor}
+          />
+        </ContextMenuItem>
+      </div>
+    </ContextMenu>
+  )
 }
