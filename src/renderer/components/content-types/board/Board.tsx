084b306a671ccb350e43cc1e1bafa80aecd34773
Peter van Hardenberg <pvh@pvh.ca>
2019-09-11 17:40:35 -0500

    move doc manipulation out

--- src/renderer/components/content-types/board/Board.tsx
+++ src/renderer/components/content-types/board/Board.tsx
@@ -1,30 +1,27 @@
 import React, { useRef, useState } from 'react'
 import Debug from 'debug'
-import uuid from 'uuid/v4'
 import { ContextMenuTrigger } from 'react-contextmenu'
 
 import ContentTypes from '../../../ContentTypes'
 import * as ImportData from '../../../ImportData'
-import { parseDocumentLink, PushpinUrl } from '../../../ShareLink'
+import { PushpinUrl } from '../../../ShareLink'
 import { ContentProps } from '../../Content'
 import { BoardDoc, BoardDocCard, CardId } from '.'
 import BoardCard from './BoardCard'
 import BoardContextMenu from './BoardContextMenu'
 import './Board.css'
+import { Position, Dimension, gridOffset, GRID_SIZE } from './BoardGrid'
 import {
-  Position,
-  Dimension,
-  gridOffset,
-  gridCellsToPixels,
-  snapDimensionToGrid,
-  snapPositionToGrid,
-  GRID_SIZE,
-} from './BoardGrid'
-import { boundPosition } from './BoardBoundary'
+  deleteCards,
+  addCardForContent,
+  moveCardsBy,
+  cardResized,
+  changeBackgroundColor,
+} from './BoardDocManipulation'
 
 import { BOARD_CARD_DRAG_ORIGIN } from '../../../constants'
-import { useDocument } from '../../../Hooks'
-import { useSelection } from './BroadcastSelection'
+import { useDocument, useStaticCallback } from '../../../Hooks'
+import { useSelection } from './BoardSelection'
 
 const log = Debug('pushpin:board')
 
@@ -67,14 +64,14 @@ export interface AddCardArgs extends CardArgs {
 export default function Board(props: ContentProps) {
   const [doc, changeDoc] = useDocument<BoardDoc>(props.hypermergeUrl)
   const boardRef = useRef<HTMLDivElement>(null)
-  const { selected, selectOnly, selectToggle, selectNone } = useSelection()
+  const { selected, selectOnly, selectToggle, selectNone } = useSelection<CardId>()
   const [distance, setDistance] = useState<Position>({ x: 0, y: 0 })
 
   const onKeyDown = (e) => {
     // this event can be consumed by a card if it wants to keep control of backspace
     // for example, see text-content.jsx onKeyDown
     if (e.key === 'Backspace') {
-      deleteCard(selected)
+      changeDoc((b) => deleteCards(b, selected))
     }
   }
 
@@ -112,8 +109,10 @@ export default function Board(props: ContentProps) {
     }
 
     ContentTypes.create('text', { text: '' }, (url) => {
-      const cardId = addCardForContent({ position, url })
-      selectOnly(cardId)
+      changeDoc((b) => {
+        const cardId = addCardForContent(b, { position, url })
+        selectOnly(cardId)
+      })
     })
   }
 
@@ -137,7 +136,7 @@ export default function Board(props: ContentProps) {
 
   const onDropInternal = (e) => {
     e.dataTransfer.dropEffect = 'move'
-    moveCardsBy(selected, distance)
+    changeDoc((b) => moveCardsBy(b, selected, distance))
   }
 
   const onDropExternal = (e) => {
@@ -153,7 +152,7 @@ export default function Board(props: ContentProps) {
     }
     ImportData.importDataTransfer(e.dataTransfer, (url, i) => {
       const position = gridOffset(dropPosition, i)
-      addCardForContent({ position, url })
+      changeDoc((b) => addCardForContent(b, { position, url }))
     })
   }
 
@@ -177,121 +176,20 @@ export default function Board(props: ContentProps) {
 
     ImportData.importDataTransfer(e.clipboardData, (url, i) => {
       const offsetPosition = gridOffset(position, i)
-      addCardForContent({ position: offsetPosition, url })
+      changeDoc((b) => addCardForContent(b, { position: offsetPosition, url }))
     })
   }
 
-  /*
-   * Card manipulation functions
-   * all the functions in this section call changeDoc
-   */
-  const addCardForContent = ({ position, dimension, url }: AddCardArgs) => {
-    const id = uuid() as CardId // ehhhhh
+  const resizeCard = useStaticCallback((id, dimension) =>
+    changeDoc((b) => cardResized(b, id, dimension))
+  )
+  const changeBackgroundColorCurried = useStaticCallback((color) =>
+    changeDoc((b) => changeBackgroundColor(b, color))
+  )
 
-    const { type } = parseDocumentLink(url)
-    const { component = {} } = ContentTypes.lookup({ type, context: 'board' }) as any
-
-    if (!dimension)
-      dimension = {
-        width: gridCellsToPixels(component.defaultWidth),
-        height: gridCellsToPixels(component.defaultHeight),
-      }
-
-    changeDoc((b: BoardDoc) => {
-      const { x, y } = snapPositionToGrid(position)
-      const { width, height } = snapDimensionToGrid(dimension)
-      const newCard: BoardDocCard = {
-        id,
-        url,
-        x,
-        y,
-      }
-      // Automerge doesn't accept undefined values,
-      // which we use to indicate content should set its own size on that dimension.
-      if (width) {
-        newCard.width = width
-      }
-      if (height) {
-        newCard.height = height
-      }
-      b.cards[id] = newCard
-    })
-
-    return id
-  }
-
-  const moveCardsBy = (selected, offset) => {
-    if (!(doc && doc.cards)) {
-      return
-    }
-    changeDoc((b) => {
-      selected.forEach((id) => {
-        const position = {
-          x: doc.cards[id].x + offset.x,
-          y: doc.cards[id].y + offset.y,
-        }
-
-        const size = {
-          width: doc.cards[id].width,
-          height: doc.cards[id].height,
-        }
-        // This gets called when uniquely selecting a card, so avoid a document
-        // change if in fact the card hasn't moved mod snapping.
-        const boundedPosition = boundPosition(position, size)
-        const newPosition = snapPositionToGrid(boundedPosition)
-
-        const cardPosition = { x: doc.cards[id].x, y: doc.cards[id].y }
-        if (newPosition.x === cardPosition.x && newPosition.y === cardPosition.y) {
-          return
-        }
-
-        const card = b.cards[id]
-        card.x = newPosition.x
-        card.y = newPosition.y
-      })
-    })
-  }
-
-  const cardResized = (id: CardId, dimension: Dimension) => {
-    if (!(doc && doc.cards)) {
-      return
-    }
-
-    // This gets called when uniquely selecting a card, so avoid a document
-    // change if in fact the card hasn't moved mod snapping.
-    const snapDimension = snapDimensionToGrid(dimension)
-    const cardDimension = { width: doc.cards[id].width, height: doc.cards[id].height }
-    if (
-      snapDimension.width === cardDimension.width &&
-      snapDimension.height === cardDimension.height
-    ) {
-      return
-    }
-
-    changeDoc((b) => {
-      const card = b.cards[id]
-      card.width = snapDimension.width
-      card.height = snapDimension.height
-    })
-  }
-
-  const deleteCard = (id) => {
-    // allow either an array or a single card to be passed in
-    if (id.constructor !== Array) {
-      id = [id]
-    }
-
-    changeDoc((b) => {
-      id.forEach((id) => delete b.cards[id])
-    })
-  }
-
-  const changeBackgroundColor = (color) => {
-    log('changeBackgroundColor')
-    changeDoc((b) => {
-      b.backgroundColor = color.hex
-    })
-  }
+  const addCardForContentCurried = useStaticCallback((addCardArgs) =>
+    changeDoc((b) => addCardForContent(b, addCardArgs))
+  )
 
   /**
    * at long last, render begins here
@@ -317,7 +215,7 @@ export default function Board(props: ContentProps) {
         uniquelySelected={uniquelySelected}
         onCardClicked={onCardClicked}
         onCardDoubleClicked={onCardDoubleClicked}
-        resizeCard={cardResized}
+        resizeCard={resizeCard}
       />
     )
   })
@@ -343,10 +241,10 @@ export default function Board(props: ContentProps) {
       <BoardContextMenu
         boardTitle={doc.title}
         contentTypes={ContentTypes.list({ context: 'board' })}
-        addCardForContent={addCardForContent}
+        addCardForContent={addCardForContentCurried}
         backgroundColor={doc.backgroundColor || BOARD_COLORS.DEFAULT}
         backgroundColors={BOARD_COLOR_VALUES}
-        changeBackgroundColor={changeBackgroundColor}
+        changeBackgroundColor={changeBackgroundColorCurried}
       />
       <ContextMenuTrigger holdToDisplay={-1} id="BoardMenu">
         <div>{cardChildren}</div>
