494c99c8059bedc8cd611c6f9703c71d0b1cbbce
Peter van Hardenberg <pvh@pvh.ca>
2019-11-09 05:25:57 -0800

    tidy up the patch a bit

--- src/renderer/components/content-types/board/BoardInBoard.tsx
+++ src/renderer/components/content-types/board/BoardInBoard.tsx
@@ -4,21 +4,20 @@ import { ContentProps } from '../../Content'
 import { BoardDoc } from '.'
 import { useDocument } from '../../../Hooks'
 import './BoardInBoard.css'
-import TitleWithSubtitle from '../../TitleWithSubtitle'
 import Badge from '../../Badge'
 import SecondaryText from '../../SecondaryText'
 import Heading from '../../Heading'
 import CenteredVerticalStack from '../../CenteredVerticalStack'
 
-BoardInBoard.minWidth = 4
+BoardInBoard.minWidth = 5
 BoardInBoard.minHeight = 6
 BoardInBoard.defaultWidth = 6
-BoardInBoard.defaultHeight = 6
+BoardInBoard.defaultHeight = 9
 BoardInBoard.maxWidth = 9
-BoardInBoard.maxHeight = 9
+BoardInBoard.maxHeight = 10
 
 export default function BoardInBoard(props: ContentProps) {
-  const { hypermergeUrl, url } = props
+  const { hypermergeUrl } = props
   const [doc] = useDocument<BoardDoc>(hypermergeUrl)
 
   if (!doc) {
@@ -31,7 +30,7 @@ export default function BoardInBoard(props: ContentProps) {
   const subTitle = `${childCardCount} card${childCardCount === 1 ? '' : 's'}`
 
   return (
-    <div className="BoardInBoard">
+    <div className="BoardInBoard BoardCard--standard">
       <CenteredVerticalStack>
         <Badge size="huge" icon="files-o" backgroundColor={backgroundColor} />
         <Heading wrap>{title}</Heading>
