ae4cd1960728683ca45538709f10fcb73c131255
Peter van Hardenberg <pvh@pvh.ca>
2019-11-09 19:12:34 -0800

    remove dead code

--- src/renderer/components/content-types/contact/ContactInVarious.tsx
+++ src/renderer/components/content-types/contact/ContactInVarious.tsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import Debug from 'debug'
-import mime from 'mime-types'
 
 import Content, { ContentProps } from '../../Content'
 import { ContactDoc } from '.'
-import { FileDoc } from '../files'
 
 import { createDocumentLink } from '../../../ShareLink'
 import { DEFAULT_AVATAR_PATH } from '../../../constants'
 
 import './ContactInVarious.css'
-import { useSelfId, useDocument, useHyperfile } from '../../../Hooks'
+import { useSelfId, useDocument } from '../../../Hooks'
 import { useContactOnlineStatus } from '../../../PresenceHooks'
 import OwnDeviceConnectionStatus from './OwnDeviceConnectionStatus'
 import ColorBadge from '../../ColorBadge'
@@ -30,31 +28,8 @@ export default function ContactInVarious(props: ContentProps) {
   const avatarDocId = contact ? contact.avatarDocId : null
   const name = contact ? contact.name : null
 
-  const [avatarImageDoc] = useDocument<FileDoc>(avatarDocId)
-
-  const { hyperfileUrl = null, extension = null } = avatarImageDoc || {}
-
-  const [header] = useHyperfile(hyperfileUrl)
-
-  const { mimeType = 'application/octet-stream' } = header || {}
-
   const isSelf = selfId === props.hypermergeUrl
   const isOnline = useContactOnlineStatus(props.hypermergeUrl)
-
-  function onDragStart(e: React.DragEvent) {
-    e.dataTransfer.setData(
-      'application/pushpin-url',
-      createDocumentLink('contact', props.hypermergeUrl)
-    )
-
-    // and we'll add a DownloadURL
-    if (hyperfileUrl) {
-      const outputExtension = extension || mime.extension(mimeType) || 'bin'
-
-      const downloadUrl = `text:${name}.${outputExtension}:${hyperfileUrl}`
-      e.dataTransfer.setData('DownloadURL', downloadUrl)
-    }
-  }
 
   if (!contact) {
     return null
