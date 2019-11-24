d984af9f5ea77235e2f3b85e418bc3edeb70c268
Peter van Hardenberg <pvh@pvh.ca>
2019-11-23 22:02:41 -0800

    remove colorbadge (unused) and an outdated CSS comment

--- src/renderer/components/ColorBadge.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './ColorBadge.css'
-
-export interface Props {
-  color: string
-}
-export default function ColorBadge({ color }: Props) {
-  return <div className="ColorBadge" style={{ backgroundColor: color }} />
-}
